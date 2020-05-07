f47f40b495e519639d4c394b718c144566cc20c5
filipjelic <filip.jelic93@gmail.com>
2020-05-07 11:27:08 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>Hello</code>
+          <code>Lorem ipusm</code>
         </p>
       </header>
     </div>
